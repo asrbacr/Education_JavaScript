939d9b13b14a4b0241fb1507b754c2846ff8bd7f
Vasiliy Gorulev <89159793029@ya.ru>
2024-04-18 20:54:33 +0300

    доделал возможность отправки кода, осталось чтобы данные собирались в строку

--- 02_JavaScript/05_DZ/усложнение/scriptUser.js
+++ 02_JavaScript/05_DZ/усложнение/scriptUser.js
@@ -33,6 +33,15 @@ function sendRequest(method, url, body = null) {
 /* sendRequest("GET", requestURL)
   .then((data) => console.log(data))
   .catch((err) => console.log(err)); */
-sendRequest("POST", requestURL, body)
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// sendRequest("POST", requestURL, body)
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+const elEl = document.querySelector("form.form");
+elEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const formProps = Object.fromEntries(formData);
+  console.log(formProps);
+  console.log(e.target);
+});
