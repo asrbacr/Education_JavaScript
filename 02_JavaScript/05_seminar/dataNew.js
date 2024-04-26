2576d073cd8a8875588b76b2aed9aeab6b583b79
Vasiliy Gorulev <89159793029@ya.ru>
2024-04-26 21:47:44 +0300

    добавил пояснение по парсингу файлов

--- 02_JavaScript/05_seminar/dataNew.js
+++ 02_JavaScript/05_seminar/dataNew.js
@@ -1,3 +1,5 @@
 fetch("https://api.nbrb.by/exrates/rates?periodicity=0")
   .then((response) => response.json())
   .then((json) => console.log(json));
+
+// fetch('../05_DZ/') чтобы получить данные из файла, то нужно использовать тоже fetch, путь указывать нужно руками. можно сгенерировать путь в index.html-е
