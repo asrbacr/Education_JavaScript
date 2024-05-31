26a6347333658fd560edbadf62f391c983299a9c
Vasiliy Gorilev <89159793029@ya.ru>
2024-05-31 12:31:58 +0300

    добавил решение домашнего задания

---
+++ 04_NodeJS/03_DZ/index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const data = path.join(__dirname, "data.json");
+
+const obj = JSON.parse(fs.readFileSync(data));
+
+app.get("/", function(req, res) {
+    obj.count_index += 1;
+    res.send(`
+    <h1>Корневая страница</h1>
+    <p>Просмотров: ${obj.count_index}</p>
+    <a href="/about">Ссылка на станицу /about</a>
+    `)
+});
+
+app.get("/about", function(req, res) {
+    obj.count_about += 1;
+    res.send(`
+    <h1>Корневая страница</h1>
+    <p>Просмотров: ${obj.count_about}</p>
+    <a href="/">Ссылка на станицу /</a>
+    `)
+});
+
+app.listen(3000);
