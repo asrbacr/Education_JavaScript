6a6f0b0257e35eb31fa1b0aa0484c7d8771ff43f
Vasiliy Gorulev <89159793029@ya.ru>
2024-06-06 22:08:24 +0300

    добавил код после семинара 4

--- 04_NodeJS/04_seminar/index.js
+++ 04_NodeJS/04_seminar/index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const Joi = require('joi');
 const app = express();
 const port = 3000;
 const pathFile = path.join(__dirname, "users.json");
+let uniqID = 3;
+
+const userSchema = Joi.object({
+  name: Joi.string().min(2).required(),
+  surname: Joi.string().min(2).required(),
+  age: Joi.number().min(0).required(),
+  city: Joi.string(),
+})
 
 app.use(express.json());
 
@@ -29,6 +38,11 @@ app.get("/users/:id", (req, res) => {
 });
 
 app.put("/users/:id", (req, res) => {
+  const resultValidation = userSchema.validate(req.body);
+  if (resultValidation.error) {
+    return res.status(400).send({error: resultValidation.error.details});
+  }
+
   const users = JSON.parse(fs.readFileSync(pathFile));
   const user = users.find((item) => item.id === +req.params.id);
   if (user) {
@@ -36,7 +50,7 @@ app.put("/users/:id", (req, res) => {
     user.surname = req.body.surname;
     user.age = req.body.age;
     user.city = req.body.city;
-    fs.writeFileSync(pathFile, JSON.stringify(users));
+    fs.writeFileSync(pathFile, JSON.stringify(users, null, 3));
     res.send({ user });
   } else {
     res
@@ -47,8 +61,37 @@ app.put("/users/:id", (req, res) => {
   res.send({ user });
 });
 
-// app.post("/", (req, res) => res.send("Hello world!"));
-// app.put("/", (req, res) => res.send("Hello world!"));
-// app.delete("/", (req, res) => res.send("Hello world!"));
+app.post("/users", (req, res) => {
+  const resultValidation = userSchema.validate(req.body);
+  if (resultValidation.error) {
+    return res.status(400).send({error: resultValidation.error.details});
+  }
+
+  const users = JSON.parse(fs.readFileSync(pathFile));
+  const user = {
+    id: uniqID++,
+    name: req.body.name,
+    surname: req.body.surname,
+    age: req.body.age,
+    city: req.body.city,
+  };
+  users.push(user);
+  fs.writeFileSync(pathFile, JSON.stringify(users, null, 3));
+  res.send({ user });
+});
+
+app.delete("/users/:id", (req, res) => {
+  const users = JSON.parse(fs.readFileSync(pathFile));
+  const userIndex = users.findIndex((item) => item.id === +req.params.id);
+  if (userIndex > -1) {
+    users.splice(userIndex, 1);
+    fs.writeFileSync(pathFile, JSON.stringify(users, null, 3));
+    res.send({ status: 'Пользователь удалён'});
+  } else {
+    res
+      .status(404)
+      .send({ user: null, error: "Пользователь не найден", status: "error" });
+  }
+});
 
 app.listen(port, () => console.log(`Сервер запущен на порту ${port}`));
