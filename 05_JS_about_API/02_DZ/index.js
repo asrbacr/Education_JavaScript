ca5aac00a22f331accbe414795172da0e267f5e7
Vasiliy Gorulev <89159793029@ya.ru>
2024-06-26 21:12:03 +0300

    делаю дз к семинару 2

--- 05_JS_about_API/02_DZ/index.js
+++ 05_JS_about_API/02_DZ/index.js
@@ -10,6 +10,39 @@
 // для быстрого переключения между изображениями.
 
 // Для создания элементов интерфейса используйте HTML.
+const data = `[
+    {
+        "id": 1,
+        "alt": "Котик 1",
+        "src": "https://uj-images.ru/images/article/2022/02/61fe96af0dc95.webp"
+    },
+    {
+        "id": 2,
+        "alt": "Котик 2",
+        "src": "https://koshka.top/uploads/posts/2021-12/1639894342_27-koshka-top-p-kotiki-na-rabochii-29.jpg"
+    },
+    {
+        "id": 3,
+        "alt": "Котик 3",
+        "src": "https://tapety.tja.pl/obrazki/tja_normalne/102432.jpg"
+    }
+]`;
+
+const arrayImageCats = JSON.parse(data);
+const borderList = document.querySelector('.border-list');
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector(".arrow-right");
+const dots = document.querySelector('.dots');
+
+console.log(arrayImageCats);
+
+
+
+arrayImageCats.forEach(elem => {
+    console.log(elem.alt);
+    console.log(elem.src);
+});
+
 // При клике на кнопку "Предыдущее изображение" 
 // должно отображаться предыдущее изображение.
 // При клике на кнопку "Следующее изображение" 
