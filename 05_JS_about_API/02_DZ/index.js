d2342b70472c8ae9bfd71e3165f3f1a2ae5db13c
Vasiliy Gorilev <89159793029@ya.ru>
2024-06-27 13:07:51 +0300

    верстаю слайдер

--- 05_JS_about_API/02_DZ/index.js
+++ 05_JS_about_API/02_DZ/index.js
@@ -1,12 +1,12 @@
-// Вашей задачей является создание веб-слайдера 
+// Вашей задачей является создание веб-слайдера
 // для отображения изображений на веб-странице.
 
-// Создайте интерфейс веб-страницы, 
+// Создайте интерфейс веб-страницы,
 // который включает в себя следующие элементы:
 // a. Контейнер для отображения текущего изображения.
-// b. Кнопки "Предыдущее изображение" и "Следующее изображение" 
+// b. Кнопки "Предыдущее изображение" и "Следующее изображение"
 // для переключения между изображениями.
-// c. Навигационные точки (индикаторы) 
+// c. Навигационные точки (индикаторы)
 // для быстрого переключения между изображениями.
 
 // Для создания элементов интерфейса используйте HTML.
@@ -29,31 +29,39 @@ const data = `[
 ]`;
 
 const arrayImageCats = JSON.parse(data);
-const borderList = document.querySelector('.border-list');
-const arrowLeft = document.querySelector('.arrow-left');
+const borderList = document.querySelector(".border-list");
+const arrowLeft = document.querySelector(".arrow-left");
 const arrowRight = document.querySelector(".arrow-right");
-const dots = document.querySelector('.dots');
+const dots = document.querySelector(".dots");
 
 console.log(arrayImageCats);
 
+const arrayTampHTML = [];
+arrayImageCats.forEach((elem) => {
+  let elemPush = `<img class="image image__none" src="${elem.src}" alt="${elem.alt}"/>`;
+  if (elem.id === 1) {
+    elemPush = `<img class="image image__active" src="${elem.src}" alt="${elem.alt}"/>`;
+  }
+  arrayTampHTML.push(elemPush);
+});
+// console.log(arrayTampHTML);
+borderList.insertAdjacentHTML("beforeend", arrayTampHTML.join(""));
 
-
-arrayImageCats.forEach(elem => {
-    console.log(elem.alt);
-    console.log(elem.src);
+arrowRight.addEventListener('click', function (e) {
+    
 });
 
-// При клике на кнопку "Предыдущее изображение" 
+// При клике на кнопку "Предыдущее изображение"
 // должно отображаться предыдущее изображение.
-// При клике на кнопку "Следующее изображение" 
+// При клике на кнопку "Следующее изображение"
 // должно отображаться следующее изображение.
-// При клике на навигационные точки, слайдер 
+// При клике на навигационные точки, слайдер
 // должен переключаться к соответствующему изображению.
 
-// Слайдер должен циклически переключаться между изображениями, 
+// Слайдер должен циклически переключаться между изображениями,
 // то есть после последнего изображения должно отображаться первое, и наоборот.
 
-// Добавьте стилизацию для слайдера и элементов интерфейса 
+// Добавьте стилизацию для слайдера и элементов интерфейса
 // с использованием CSS для улучшения внешнего вида.
 
 // Примеры модальных окон:
