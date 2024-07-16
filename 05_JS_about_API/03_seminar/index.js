7a4df89a53ed338ef35590262edcc0b9de16aa50
Vasiliy Gorilev <89159793029@ya.ru>
2024-07-16 13:05:22 +0300

    добавляю код с семинара 3. время записи 46:44

--- 05_JS_about_API/03_seminar/index.js
+++ 05_JS_about_API/03_seminar/index.js
@@ -1,4 +1,4 @@
-// Задача 1.
+/* // Задача 1.
 
 // Дан массив товаров:
 
@@ -78,15 +78,64 @@ productsData = [
 // Стилизуйте элементы интерфейса с помощью CSS для улучшения
 // внешнего вида.
 
-function displatCatalog (selectCategory) {
-    productsData.forEach(elem => {
-        
-    });
+function displayCatalog(selectCategory) {
+   productHTML = ``;
+  productsData.forEach((elem) => {
+    if (selectCategory === "" || elem.category === selectCategory) {
+      productHTML += createCard(elem);
+    }
+  });
+  wrapperEl.innerHTML = productHTML;
 }
 
+function createCard(cardInfo) {
+  return `
+      <div class="card">
+        <div class="card__h1">${cardInfo.id}</div>
+        <div class="card__name">${cardInfo.name}</div>
+        <div class="card__category">${cardInfo.category}</div>
+     </div>`;
+}
+
+const wrapperEl = document.querySelector("div.wrapper");
 const categorySelectEl = document.querySelector("#categorySelect");
 categorySelectEl.addEventListener("change", () => {
   let selectCategory = categorySelectEl.value;
   console.log(selectCategory);
-  displatCatalog(selectCategory);
-});
+  displayCatalog(selectCategory);
+}); */
+
+// Задача 2.
+ 
+// Бесконечная лента фотографий
+// Для создания бесконечной ленты с фотографиями с использованием 
+// Unsplash API, выполните следующие шаги:
+// 1. Зарегистрируйтесь на Unsplash:
+// ○ Перейдите на веб-сайт Unsplash (https://unsplash.com/).
+// ○ Нажмите кнопку "Join" или "Регистрация", чтобы создать аккаунт, если у вас его еще нет.
+// ○ Войдите в свой аккаунт Unsplash.
+ 
+// 2. Создайте приложение:
+// ○ После входа в аккаунт Unsplash, перейдите на страницу разработчика Unsplash
+// (https://unsplash.com/developers).
+// ○ Нажмите на кнопку "Your apps".
+// ○ Нажмите "New Application" (Новое приложение).
+// ○ Заполните информацию о вашем приложении, такую как имя, описание и сайт (вы можете
+// использовать http://localhost для тестового сайта).
+// ○ После заполнения информации, нажмите "Create Application" (Создать приложение).
+ 
+// 3. Получите API-ключ:
+// ○ После создания приложения, вы получите API-ключ. Этот ключ будет
+// отображаться в вашей панели управления приложением.
+// ○ API-ключ представляет собой строку вида YOUR_ACCESS_KEY.
+// Скопируйте его.
+// 4. Измените код HTML и JavaScript:
+// ○ Откройте вашу HTML-страницу в текстовом редакторе или
+// интегрированной среде разработки.
+// ○ Замените 'YOUR_ACCESS_KEY' в коде JavaScript на ваш собственный
+// API-ключ.
+ 
+// 5. Реализуйте загрузку фотографий при открытии страницы.
+ 
+// 6. Реализуйте бесконечную подгрузку фотографий при прокручивании страницы.
+ 
