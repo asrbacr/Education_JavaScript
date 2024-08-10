539f850a8d497f521a2caeffca6343b00e4da1d5
Vasiliy Gorulev <89159793029@ya.ru>
2024-08-10 12:07:05 +0300

    смотрю запись семинара 3. время 1:24:00

---
+++ 06_JS_advanced/03_seminar/task_2.js
@@ -0,0 +1,40 @@
+/*
+Задание 2: 
+Вы разрабатываете онлайн-магазин по заказу индивидуальных мебельных комплектов. 
+Посетители сайта могут выбирать разные элементы мебели, цвета и материалы для 
+своего гарнитура. После того как пользователь собрал свой комплект и сохраняет 
+свой выбор, информация о нем сохраняется в localStorage. При следующем посещении 
+сайта их индивидуальные настройки автоматически загружаются, и они видят ранее 
+созданный мебельный комплект.
+ 
+1. Создайте базовую HTML-структуру с различными элементами мебели (например, 
+стол, стул, диван) и возможными параметрами для них (цвет, материал, дизайн).
+2. Пользователи могут выбирать разные элементы и параметры, чтобы составить свой
+мебельный гарнитур.
+3. После выбора всех желаемых параметров предоставьте кнопку "Сохранить 
+комплект", которая сохраняет текущий выбор пользователя в localStorage.
+4. При следующем посещении сайта автоматически загрузите сохраненные параметры 
+из localStorage и отобразите ранее созданный комплект.
+5. Убедитесь, что у пользователей есть возможность очистить сохраненные 
+настройки (очистить localStorage).
+*/
+const saveButtonEl = document.getElementById("save-btn");
+const loadButtonEl = document.getElementById("load-btn");
+const clearButtonEl = document.getElementById("clear-btn");
+
+const localStorageKey = "settings";
+const tableColorEl = document.querySelector("#table-color");
+const chairMaterialEl = document.querySelector("#chair-material");
+
+saveButtonEl.addEventListener("click", (e) => {
+  const color = tableColorEl.value;
+  const material = chairMaterialEl.value;
+  localStorage.setItem(localStorageKey, JSON.stringify({ color, material }));
+});
+
+if(localStorage.getItem(localStorageKey)) {
+    const localStorageData = JSON.parse(localStorage.getItem(localStorageKey));
+    // console.log(localStorageData);
+    tableColorEl.value = localStorageData.color;
+    chairMaterialEl.value = localStorageData.material;
+}
