ffb6b2846bce41f028352c92750004bdbd7ba9bf
Vasiliy Gorilev <89159793029@ya.ru>
2024-08-16 13:01:58 +0300

    делаю задание

---
+++ 06_JS_advanced/03_DZ/list-feedback.js
@@ -0,0 +1,4 @@
+import { renderFeedback } from "./storage.js";
+
+const containerEl = document.querySelector(".container");
+renderFeedback(containerEl);
--- 06_JS_advanced/03_DZ/storage.js
+++ 06_JS_advanced/03_DZ/storage.js
@@ -1,9 +1,9 @@
 const lsName = "name-product";
 const lsText = "text-feedback";
 const lsFeedbacks = "feedbacks";
-const feedback = [];
 
 function setFeedback(name, text) {
+  const feedback = getFeedback();
   const nameValue = name.value;
   const textValue = text.value;
 
@@ -17,4 +17,50 @@ function setFeedback(name, text) {
   localStorage.setItem(lsFeedbacks, JSON.stringify(feedback));
 }
 
-export { setFeedback };
+function getFeedback() {
+  const data = localStorage.getItem(lsFeedbacks);
+  if (!data) {
+    return [];
+  }
+  return JSON.parse(data);
+}
+
+function renderFeedback(container) {
+  const dataHTML = "";
+  const feedbackList = getFeedback();
+  console.log(feedbackList);
+  feedbackList.forEach(elemList => {
+    console.log(feedbackCart(elemList));
+    // dataHTML += feedbackCart(elemList);
+  });
+
+  console.log(dataHTML);
+  // container.innerHTML = feedbackCart(dataHTML);
+  console.log(container);
+}
+
+function feedbackList(el) {
+  let cartListHTML = ``;
+  el.forEach((elLi) => {
+    cartListHTML += `<li class="feedback__li">${elLi.text}</li>`;
+  });
+  return cartListHTML;
+}
+
+function feedbackCart(el) {
+  return `
+  ${el[lsName]} ${el[lsText]}
+  `
+  // return `<div class="feedback">
+  //     <h3 class="product">${[lsName].el}</h3>
+  //     <ul class="feedback__list">
+  //       ${feedbackList(el)}
+  //     </ul>
+
+  //     <form action="#">
+  //       <input class="feedback__input" type="text" name="add_feedback" required />
+  //       <input class="feedback__button" type="button" value="Добавить отзыв" />
+  //     </form>
+  //   </div>`;
+}
+export { setFeedback, getFeedback, renderFeedback };
