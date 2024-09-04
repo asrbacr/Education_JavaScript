a1cae4ffccae36d5a77c2add63874ad299190717
Vasiliy Gorulev <89159793029@ya.ru>
2024-09-04 22:14:40 +0300

    добавил файлы после семинара 1

--- 07_React/01_seminar/my-first-react-app/src/App.js
+++ 07_React/01_seminar/my-first-react-app/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greeting from "./components/Greeting/Greeting.jsx";
+import CurrentTime from "./components/CurrentTime";
+import EventCard from "./components/EventCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Greeting />
+        <CurrentTime />
+        <EventCard name="Кошки" date="05.09" location="г. Ярославль" />
       </header>
     </div>
   );
