c13803ecb802dc71a8b69d9f6ff73d01a48cb9c6
Vasiliy Gorilev <89159793029@ya.ru>
2024-09-05 12:38:14 +0300

    добавил решение ДЗ 1

---
+++ 07_React/01_DZ/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+import { Message } from './components/Message'
+
+function App() {
+
+  return (
+    <>
+      <Message text="Василий Горулев" />
+    </>
+  )
+}
+
+export default App
---
+++ 07_React/01_DZ/src/components/Message.jsx
@@ -0,0 +1,8 @@
+export const Message = (props) => {
+    return ( 
+        <div className="logo">
+            <h1 className="">{props.text}</h1>
+            <h3>Добро пожаловать в начало изучения React!</h3>
+        </div>
+     );
+}
