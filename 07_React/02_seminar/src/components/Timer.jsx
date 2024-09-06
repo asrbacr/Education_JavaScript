f50b606b40446b16855d9927890e7bd1a95540ce
Vasiliy Gorilev <89159793029@ya.ru>
2024-09-06 12:59:24 +0300

    дописал код с семинара 2

--- 07_React/02_seminar/src/components/Timer.jsx
+++ 07_React/02_seminar/src/components/Timer.jsx
@@ -4,48 +4,19 @@ export function Timer() {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    const time = setInterval(
-      (() => {
+    const timer = setInterval(() => {
         setTime((prevTime) => prevTime + 1);
       },
       1000)
-    );
 
     return () => {
-      clearInterval(time);
+      clearInterval(timer);
     };
   }, []);
-
   return (
     <div>
       <p>Таймер: {time}</p>
     </div>
   );
+
 }
-
-/* import { useEffect, useState } from "react";
-
-function Timer() {
-  const [time, setTime] = useState(0);
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
-    }, 1000);
-    // что должно выполнится
-
-    return () => {
-      clearInterval(timer);
-      // в момент времени какой-то (он может отсутствовать)
-    };
-  }, []); // внутри массива значение пропса которое заставит выполнится функцию только в моменнт обновления пропса (но тут у нас пусто)
-  // это значит что функция будет работать и в момент монтирования и в моент обновления
-
-  return (
-    <div>
-      <p>Время: {time}</p>
-    </div>
-  );
-}
-
-export default Timer; */
