9f76508f05f9159cb4a33c27608f98dc6976bfec
Vasiliy Gorilev <89159793029@ya.ru>
2024-09-19 13:07:31 +0300

    делаю ДЗ к семинару 4

--- 07_React/04_DZ/src/components/ParseJson/ParseJson.jsx
+++ 07_React/04_DZ/src/components/ParseJson/ParseJson.jsx
@@ -11,30 +11,26 @@ const getData = async (url) => {
   };
 
 export const ParseJson = ({data}) => {
-    const overshootCart = (e) => {
-            e.options.map((cart, i) => (
-                return (
-                    <li key={i}>
-                        <img src={cart.image_product} alt={elem.name} />
-                        <p>{cart.weight}</p>
-                        <p>{cart.price}</p>
-                    </li>
-                )
-            )
-        )
-    }
 
     return (
         <div className={st.cards}>
-            {data.map((elem)=>{
+            {data.map((elem)=> {
                 return (
-                    <div key={elem.id} className={st.card}>
-                        <h2>{elem.name}</h2>
-                        <img src={elem.image_group} alt={elem.name} className={st.img}/>
-                        <ul>
-                            {}
-                        </ul>
-                    </div>
+                    <details>
+                        <summary key={elem.id} className={st.card}>
+                            <h2>{elem.name}</h2>
+                            <img src={elem.image_group} alt={elem.name} className={st.img}/>
+                        </summary>
+                        {elem.option.map((cart, i)=> (
+                            <div key={i} className={st.li__list}>
+                                <img src={cart.image_product} alt={elem.name} className={st.img} />
+                                <div>
+                                    <p>Вес: {cart.weight} г</p>
+                                    <p>Цена: {cart.price},00 руб</p>
+                                </div>
+                            </div>
+                        ))}
+                    </details>
                 )
             })}
         </div>
