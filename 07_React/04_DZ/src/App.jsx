2a73fff5b92514fb1532ac55efce89bce32e9494
Vasiliy Gorilev <89159793029@ya.ru>
2024-09-26 12:37:04 +0300

    сделал задание 4

--- 07_React/04_DZ/src/App.jsx
+++ 07_React/04_DZ/src/App.jsx
@@ -1,22 +1,22 @@
 import s from "./App.module.css";
 import { Route, BrowserRouter, Routes, NavLink } from "react-router-dom";
-import { WeightList } from "./components/CardProduct/CardProduct";
+import { CardLists, DetailPage } from "./components/CardProduct/CardProduct";
 import data from "../data.json";
 
 function App() {
   return (
     <>
-      {/*       <BrowserRouter>
+      <BrowserRouter>
         <nav className={s.nav}>
           <NavLink to="/">Главная</NavLink>
           <NavLink to="/product">Группы товаров</NavLink>
         </nav>
         <Routes>
-          <Route path='/' element={<Header />} />
+          <Route path='/product' element={<CardLists items={data} />} />
+          <Route path='/product/:id' element={<DetailPage items={data} />} />
         </Routes>
-      </BrowserRouter> */}
-      {console.log(data)}
-      <WeightList items={data} />
+      </BrowserRouter>
+      
     </>
   );
 }
--- 07_React/04_DZ/src/components/CardProduct/CardProduct.jsx
+++ 07_React/04_DZ/src/components/CardProduct/CardProduct.jsx
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import s from "./CardProduct.module.scss";
+import { Link, useParams } from "react-router-dom";
 
 export const ElemCardInList = ({ item }) => {
   return (
     <div className={[s.border]}>
       <img src={item.image_group} alt={item.name} className={s.img} />
       <h2>{item.name}</h2>
+      <Link to={`/product/${item.id}`}>Варианты веса</Link>
     </div>
   );
 };
@@ -36,9 +38,6 @@ export const CardWeight = ({item}) => {
 export const CardWeightList = ({ items }) => {
   return (
     <div>
-      <h2>{items.name}</h2>
-      <img src={items.image_group} alt={items.name} className={s.img} />
-
       <ul>
         {items.option.map((item) => (
           <CardWeight item={item} />
@@ -48,12 +47,13 @@ export const CardWeightList = ({ items }) => {
   );
 };
 
-export const WeightList = ({ items }) => {
+export const DetailPage = ({ items }) => {
+  const { id } = useParams();
+  const product = items.find(product => product.id === id);
+
   return (
-    <div>
-      {items.map((product) => (
-        <CardWeightList key={product.id} items={product} />
-      ))}
-    </div>
-  );
-};
+      <>
+        <CardWeightList items={product} />
+      </>
+  )
+}
